7d824c431c19b4310b87565a47ec0af52dc7efdd
Kyzderp <amurtiger224@gmail.com>
2021-03-20 14:01:45 -0700

    Add tab switching

--- ui/quickstars.lua
+++ ui/quickstars.lua
@@ -11,24 +11,44 @@ local function GetFlippedSlottables()
     return flipped
 end
 
+
 ---------------------------------------------------------------------
--- On move stop
-function DynamicCP.SaveQuickstarsPosition()
-    DynamicCP.savedOptions.quickstarsX = DynamicCPQuickstars:GetLeft()
-    DynamicCP.savedOptions.quickstarsY = DynamicCPQuickstars:GetTop()
+-- When user switches tab, we should update backdrops and dropdowns
+
+-- Set backdrops to reflect which tab is selected
+local function SetBackdrops(tree)
+    local colors = {
+        Green = {0.55, 0.78, 0.22, 1},
+        Blue = {0.35, 0.73, 0.9, 1},
+        Red = {0.88, 0.4, 0.19, 1},
+    }
+    local color = colors[tree]
+    DynamicCPQuickstarsListBackdrop:SetCenterColor(unpack(color))
+    DynamicCPQuickstarsListConfirmBackdrop:SetCenterColor(unpack(color))
+    DynamicCPQuickstarsListCancelBackdrop:SetCenterColor(unpack(color))
+
+    DynamicCPQuickstarsGreenButtonBackdrop:SetCenterColor(0, 0, 0, 1)
+    DynamicCPQuickstarsBlueButtonBackdrop:SetCenterColor(0, 0, 0, 1)
+    DynamicCPQuickstarsRedButtonBackdrop:SetCenterColor(0, 0, 0, 1)
+    DynamicCPQuickstars:GetNamedChild(tree .. "Button"):GetNamedChild("Backdrop"):SetCenterColor(unpack(color))
 end
 
----------------------------------------------------------------------
--- Init
-function DynamicCP.InitQuickstars()
-    DynamicCPQuickstars:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, DynamicCP.savedOptions.quickstarsX, DynamicCP.savedOptions.quickstarsY)
-    -- TODO: show setting
+-- Reinitialize dropdowns for the particular tree
+local function UpdateDropdowns(tree)
+    local offsets = {
+        Green = 0,
+        Blue = 4,
+        Red = 8,
+    }
 
+    local offset = offsets[tree]
     local committed = GetFlippedSlottables()
     for i = 1, 4 do
         local dropdown = ZO_ComboBox_ObjectFromContainer(DynamicCPQuickstarsList:GetNamedChild("Star" .. tostring(i)))
-        local name = zo_strformat("<<C:1>>", GetChampionSkillName(committed[i]))
+        dropdown:ClearItems()
 
+        local currentSkillId = committed[offset + i]
+        local name = zo_strformat("<<C:1>>", GetChampionSkillName(currentSkillId))
 
         local function OnItemSelected(_, _, entry)
             DynamicCP.dbg("Selected " .. tostring(entry))
@@ -38,4 +58,36 @@ function DynamicCP.InitQuickstars()
         dropdown:UpdateItems()
         dropdown:SelectItem(entry)
     end
+end
+
+-- Called when user clicks tab button
+function DynamicCP.SelectTab(tree)
+    DynamicCP.dbg("clicked " .. tree)
+    -- Set backdrops to appropriate color
+    SetBackdrops(tree)
+
+    -- Update dropdowns
+    UpdateDropdowns(tree)
+
+    -- Hide confirm/cancel buttons
+    DynamicCPQuickstarsListConfirm:SetHidden(true)
+    DynamicCPQuickstarsListCancel:SetHidden(true)
+end
+
+
+---------------------------------------------------------------------
+-- On move stop
+function DynamicCP.SaveQuickstarsPosition()
+    DynamicCP.savedOptions.quickstarsX = DynamicCPQuickstars:GetLeft()
+    DynamicCP.savedOptions.quickstarsY = DynamicCPQuickstars:GetTop()
+end
+
+
+---------------------------------------------------------------------
+-- Init
+function DynamicCP.InitQuickstars()
+    DynamicCPQuickstars:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, DynamicCP.savedOptions.quickstarsX, DynamicCP.savedOptions.quickstarsY)
+    -- TODO: show setting
+
+    DynamicCP.SelectTab("Green")
 end
