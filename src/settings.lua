8e07a15b922cef333ede1667640989e335b743c6
Kyzderp <amurtiger224@gmail.com>
2021-04-04 07:08:16 -0700

    Fix autoslot stars settings text

--- src/settings.lua
+++ src/settings.lua
@@ -249,7 +249,7 @@ function DynamicCP:CreateSettingsMenu()
                 {
                     type = "checkbox",
                     name = "Automatically slot stars",
-                    tooltip = "After confirming a preset, automatically slot slottable stars if there are 4 or fewer unlocked slottables",
+                    tooltip = "After confirming a preset, automatically slot slottable stars. If there are more than 4 slottables, they are prioritized by most maxed, then most points, then star index",
                     default = true,
                     getFunc = function() return DynamicCP.savedOptions.slotStars end,
                     setFunc = function(value)
@@ -257,18 +257,6 @@ function DynamicCP:CreateSettingsMenu()
                     end,
                     width = "full",
                 },
-                -- {
-                --     type = "checkbox",
-                --     name = "Automatically slot higher stars",
-                --     tooltip = "After confirming a preset, if there are 5 or more unlocked slottables, slot the 4 that are closest to max. If there are more than 4, the order is determined by the star's index",
-                --     default = true,
-                --     getFunc = function() return DynamicCP.savedOptions.slotHigherStars end,
-                --     setFunc = function(value)
-                --         DynamicCP.savedOptions.slotHigherStars = value
-                --     end,
-                --     width = "full",
-                --     disabled = function() return not DynamicCP.savedOptions.slotStars end
-                -- },
             },
         },
 ---------------------------------------------------------------------
