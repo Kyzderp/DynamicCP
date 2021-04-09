cedd78f47818178b29a0ca3e51090fad7f1dee67
Kyzderp <amurtiger224@gmail.com>
2021-04-08 20:40:05 -0700

    Fix gained message for multiple points

--- src/init.lua
+++ src/init.lua
@@ -134,8 +134,17 @@ local function RegisterEvents()
         function(_, championPointsDelta)
             -- Show CP gained message
             if (DynamicCP.savedOptions.showPointGainedMessage) then
-                CHAT_SYSTEM:AddMessage("|cAAAAAAGained "
-                    .. ZO_CenterScreenAnnounce_GetEventHandler(EVENT_CHAMPION_POINT_GAINED)(1).secondaryText .. "|r")
+                DynamicCP.dbg("Gained " .. tostring(championPointsDelta))
+                -- This is what the game code uses to generate the center announcement text,
+                -- but it's actually kinda buggy. If you gain more than 1 point at once, the
+                -- text shows the wrong tree gained because it's already updated.
+                -- We'll live with that for now though...
+                local gainedString = ZO_CenterScreenAnnounce_GetEventHandler(EVENT_CHAMPION_POINT_GAINED)(championPointsDelta).secondaryText
+                gainedString = string.gsub(gainedString, "\n", " ")
+                if (championPointsDelta > 1) then
+                    gainedString = tostring(championPointsDelta) .. " points: " .. gainedString
+                end
+                CHAT_SYSTEM:AddMessage("|cAAAAAAGained " .. gainedString .. "|r")
             end
 
             -- Update totals label
