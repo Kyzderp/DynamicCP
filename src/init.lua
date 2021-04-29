c2c5a5be85f23a3c9b70fef7bc2a00826da76b98
Kyzderp <amurtiger224@gmail.com>
2021-04-29 05:05:01 -0700

    [0.7.1] Version bump

--- src/init.lua
+++ src/init.lua
@@ -1,6 +1,6 @@
 DynamicCP = DynamicCP or {}
 DynamicCP.name = "DynamicCP"
-DynamicCP.version = "0.7.0"
+DynamicCP.version = "0.7.1"
 
 DynamicCP.experimental = false -- Flip to true when developing. Nothing currently
 
