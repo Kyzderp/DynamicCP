88ced7898559e6b9e80b60667405c53052ce4879
Kyzderp <amurtiger224@gmail.com>
2021-06-17 17:46:02 -0700

    [1.1.3] Fix save new preset

--- ui/presets.lua
+++ ui/presets.lua
@@ -341,11 +341,13 @@ function DynamicCP:OnSaveClicked(button, tree)
         newCP[disciplineIndex][k] = v
     end
     -- Also copy the other things like role and class
-    for index, _ in pairs(DynamicCP.savedOptions.cp[tree][presetName]) do
-        if (index ~= disciplineIndex) then
-            newCP[index] = {}
-            for k, v in pairs(DynamicCP.savedOptions.cp[tree][presetName][index]) do
-                newCP[index][k] = v
+    if (DynamicCP.savedOptions.cp[tree][presetName]) then
+        for index, _ in pairs(DynamicCP.savedOptions.cp[tree][presetName]) do
+            if (index ~= disciplineIndex and type(index) ~= "number") then
+                newCP[index] = {}
+                for k, v in pairs(DynamicCP.savedOptions.cp[tree][presetName][index]) do
+                    newCP[index][k] = v
+                end
             end
         end
     end
