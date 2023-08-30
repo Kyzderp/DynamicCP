941cdbad37c3ef8ce483aa994e998f1e6ca060da
Kyzderp <amurtiger224@gmail.com>
2023-08-30 16:50:19 -0700

    [2.0.16] Fix error on armory load in combat

--- src/init.lua
+++ src/init.lua
@@ -1,6 +1,6 @@
 DynamicCP = DynamicCP or {}
 DynamicCP.name = "DynamicCP"
-DynamicCP.version = "2.0.15"
+DynamicCP.version = "2.0.16"
 
 DynamicCP.experimental = false -- Flip to true when developing. Nothing currently
 
