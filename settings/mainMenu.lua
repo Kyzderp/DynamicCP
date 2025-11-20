71febe0caa3b62ddb53666d4dd687633923d27d5
Kyzeragon <amurtiger224@gmail.com>
2025-11-20 09:10:55 -0800

    Delete unused settings

--- settings/mainMenu.lua
+++ settings/mainMenu.lua
@@ -2,8 +2,8 @@ function DynamicCP:CreateSettingsMenu()
     local LAM = LibAddonMenu2
     local panelData = {
         type = "panel",
-        name = "Dynamic CP",
-        displayName = "|c3bdb5eDynamic CP|r",
+        name = "Dynamic CP Lite",
+        displayName = "|c3bdb5eDynamic CP Lite|r",
         author = "Kyzeragon",
         version = DynamicCP.version,
         registerForRefresh = true,
@@ -11,17 +11,6 @@ function DynamicCP:CreateSettingsMenu()
     }
 
     local optionsData = {
-        {
-            type = "checkbox",
-            name = "Debug",
-            tooltip = "Show spammy debug chat",
-            default = false,
-            getFunc = function() return DynamicCP.savedOptions.debug end,
-            setFunc = function(value)
-                DynamicCP.savedOptions.debug = value
-            end,
-            width = "full",
-        },
 ---------------------------------------------------------------------
 -- constellation
         {
@@ -158,59 +147,6 @@ function DynamicCP:CreateSettingsMenu()
                     setFunc = function(value)
                         DynamicCP.savedOptions.showTotalsLabel = value
                         DynamicCPInfoLabel:SetHidden(not value)
-                    end,
-                    width = "full",
-                },
-            },
-        },
----------------------------------------------------------------------
--- presets window
-        {
-            type = "submenu",
-            name = "Preset Window Settings",
-            controls = {
-                {
-                    type = "checkbox",
-                    name = "Show window with CP",
-                    tooltip = "Display the window automatically when opening the Champion Points menu. You can turn this off if you do not wish to use presets",
-                    default = true,
-                    getFunc = function() return DynamicCP.savedOptions.showPresetsWithCP end,
-                    setFunc = function(value)
-                        DynamicCP.savedOptions.showPresetsWithCP = value
-                    end,
-                    width = "full",
-                },
-                {
-                    type = "slider",
-                    name = "Window scale %",
-                    tooltip = "Scale of the window to display. Some spacing may look weird especially at more extreme values",
-                    default = 100,
-                    min = 50,
-                    max = 150,
-                    step = 5,
-                    getFunc = function() return DynamicCP.savedOptions.scale * 100 end,
-                    setFunc = function(value)
-                        DynamicCP.savedOptions.scale = value / 100
-                        DynamicCPPresetsContainer:SetHidden(false)
-                        DynamicCP.GetSubControl():SetHidden(false)
-                        DynamicCPSidePresets:SetScale(value / 100)
-                    end,
-                    width = "full",
-                },
-                {
-                    type = "slider",
-                    name = "Window opacity %",
-                    tooltip = "Opacity of the window background",
-                    default = 50,
-                    min = 0,
-                    max = 100,
-                    step = 5,
-                    getFunc = function() return DynamicCP.savedOptions.presetsBackdropAlpha * 100 end,
-                    setFunc = function(value)
-                        DynamicCP.savedOptions.presetsBackdropAlpha = value / 100
-                        DynamicCPPresetsContainer:SetHidden(false)
-                        DynamicCP.GetSubControl():SetHidden(false)
-                        DynamicCPSidePresetsBackdrop:SetAlpha(value / 100)
                     end,
                     width = "full",
                 },
@@ -369,41 +305,6 @@ function DynamicCP:CreateSettingsMenu()
                     end,
                     width = "full",
                 },
-                {
-                    type = "checkbox",
-                    name = "Play sound",
-                    tooltip = "Play the champion points committed sound when changing slottables",
-                    default = true,
-                    getFunc = function() return DynamicCP.savedOptions.quickstarsPlaySound end,
-                    setFunc = function(value)
-                        DynamicCP.savedOptions.quickstarsPlaySound = value
-                    end,
-                    width = "full",
-                },
-                {
-                    type = "checkbox",
-                    name = "Show cooldown",
-                    tooltip = "Show a small label with a countdown indicating the 30-second cooldown on changing slottables",
-                    default = true,
-                    getFunc = function() return DynamicCP.savedOptions.quickstarsShowCooldown end,
-                    setFunc = function(value)
-                        DynamicCP.savedOptions.quickstarsShowCooldown = value
-                    end,
-                    width = "half",
-                },
-                {
-                    type = "colorpicker",
-                    name = "Cooldown label color",
-                    tooltip = "Color of the label for 30-second cooldown on Quickstars panel",
-                    default = ZO_ColorDef:New(0.7, 0.7, 0.7),
-                    getFunc = function() return unpack(DynamicCP.savedOptions.quickstarsCooldownColor) end,
-                    setFunc = function(r, g, b)
-                        DynamicCP.savedOptions.quickstarsCooldownColor = {r, g, b}
-                        DynamicCPQuickstarsPanelCooldown:SetColor(unpack(DynamicCP.savedOptions.quickstarsCooldownColor))
-                    end,
-                    width = "half",
-                    disabled = function() return not DynamicCP.savedOptions.quickstarsShowCooldown end
-                },
             },
         },
 ---------------------------------------------------------------------
@@ -463,64 +364,14 @@ function DynamicCP:CreateSettingsMenu()
 -- preset application
         {
             type = "submenu",
-            name = "Default Presets",
+            name = "Automatic Presets",
             controls = {
-                {
-                    type = "header",
-                    name = "Automatic Presets",
-                    width = "full",
-                },
                 {
                     type = "description",
                     text = "Version 3.0 adds auto presets! These are the brighter green options at the top of the preset lists, with icons showing what CP they will prioritize. They will use as many points as you have available, until stars useful for your detected build are all allocated. The current presets are:\n\n|c7f9c4f- Auto Combat: prioritizes stars useful in dungeons or trials\n- Auto Craft/Loot: prioritizes crafting and overland looting\n- Auto Thieving: prioritizes nefarious activities\n|c5096b3- Auto PvE: allocates based on your selected LFG role, and if you are a DPS, also allocates based on your max resource and whether you are a heavy attack build (Sergeant's Mail), have Fatecarver or jabs or Blighted Blastbones unlocked (for AoE) or not (single target)\n|cb56238- Auto PvE: allocates based on your selected LFG role, and if you are a DPS, also allocates based on whether you have Pragmatic Fatecarver unlocked (Bastion) or not (Rejuvenation)|r\n\nThe green tree presets above also try to skip Inspiration Boost if your crafting skills are maxed, unless you don't have enough points to reach stars without Inspiration Boost.",
                     width = "full",
                 },
-                {
-                    type = "header",
-                    name = "Old Default (Hardcoded) Presets",
-                    width = "full",
-                },
-                {
-                    type = "description",
-                    title = "Manage Default (Hardcoded) Presets",
-                    text = "You can delete ALL of the default hardcoded presets, from pre-3.0, using the button below. This will delete any presets that match the names of the default presets, so be careful if you made changes to any!",
-                    width = "full",
-                },
-                {
-                    type = "button",
-                    name = "Delete Default Presets",
-                    tooltip = "Delete all default presets. This cannot be undone!",
-                    func = function()
-                        DynamicCP.dbg("Deleting default presets")
-                        for treeName, tree in pairs(DynamicCP.savedOptions.cp) do
-                            for name, data in pairs(tree) do
-                                if (DynamicCP.oldDefaultPresetNames[name] or data.isDefault == true) then
-                                    DynamicCP.savedOptions.cp[treeName][name] = nil
-                                end
-                            end
-                        end
-                        DynamicCP:InitializeDropdowns()
-                    end,
-                    warning = "Delete all default presets. This cannot be undone!",
-                    isDangerous = true,
-                    width = "full",
-                },
             },
-        },
----------------------------------------------------------------------
-        {
-            type = "header",
-            name = "",
-            width = "full",
-        },
-        {
-            type = "button",
-            name = "Show latest changelog",
-            tooltip = "Show the changelog from the last major version",
-            func = function()
-                DynamicCP.MaybeShowChangelog(true)
-            end,
-            width = "full",
         },
     }
 
